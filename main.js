ed0fa10daf4e48f95abfbe1778d78bb134a513d5
Gary Siu <Gary@untypeable.com>
2015-08-17 00:04:35 +0100

    basic alternating player logic

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var clickTarget = document.getElementsByClassName('result');
+function getPlayer() {
+  var player;
+  document.getElementById('turn').innerText === "Player 1's turn" ? player = 0 : player = 1;
+  return player;
+}
+function togglePlayer(){
+  getPlayer() === 0 ? document.getElementById('turn').innerText = "Player 2's turn" :
+  document.getElementById('turn').innerText = "Player 1's turn";
+}
 for (var i = 0; i < clickTarget.length; i++) {
   clickTarget[i].addEventListener('click', function(){
-    this.style.background = "blue";
+    switch (getPlayer()) {
+      case 0 : this.style.background = "blue"; togglePlayer();
+      break;
+      default: this.style.background = "red"; togglePlayer();
+      break;
+    }
   });
 }
